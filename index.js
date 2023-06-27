9d91ae99b9312e34a32a2a9248f7fc550d3ee766
Tobobus <118485372+tobobus213@users.noreply.github.com>
2023-06-27 09:44:02 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+let firstName_Mal = ['Gabriel', 'Alexandre', 'Adam', 'Raphaël', 'Louis', 'Paul', 'Antoine', 'Maxime', 'Thomas', 'Victor', 'Lucas', 'Jules', 'Nathan', 'Hugo', 'Sacha', 'Arthur', 'Mohamed', 'Enzo', 'Gaspard', 'Clement', 'Samuel', 'Ethan', 'Leo', 'Adrien', 'Martin', 'Rayan', 'Baptiste', 'Yanis', 'Simon', 'Joseph', '  Mathis', '  Oscar', '  Alexis', '  Pierre', '  Noah', '  Axel', '  Theo', '  Romain', '  Valentin', '  Augustin', '  Julien', '  Nicolas', '  Maxence', '  Noe', ' Benjamin', ' Quentin', ' Tom', ' Charles', ' David', ' Jean',
+            'Eliott', 'Aaron', 'Elias', 'Matthieu', 'Tristan', 'Timothee', 'Mathieu', 'Matteo', 'William', 'mine', 'Felix', 'Côme', 'Ruben', 'Ulysse', 'Antonin', 'Guillaume', 'Aurelien', '  Robin', '  Maël', '  Kevin', '  Mehdi', '  Ismaël', '  Liam', '  Gabin', '  Noam', '  Evan', '  Marius', '  Mathias', '  Isaac', '  Samy', '  Ibrahim', '  Basile', '  Daniel', '  Emile', '  Lucien', '  Elie', '  Rayane', '  Leonard',
+            'Thibault', 'Achille', 'Leon', 'Edouard', 'Hadrien', 'Lenny', 'Matheo', 'Malo', 'Titouan', 'Solal', 'Joshua', '  Max', '  Gregoire', '  Vincent', 'Youssef', '  Luca', '  Esteban', '  Yacine', '  Florian', '  Corentin', '  Amaury', '  Edgar', '  Yassine', '  Roman', '  Milo', '  Octave', '  Hector', '  Ahmed', '  Hippolyt',
+            'Dylan', 'Ayoub', 'Emmanuel', 'Nolan', 'Abel', 'Yann', 'Mamadou', 'Ali', 'Anatole', 'Etienne', 'Naël', 'Anis', 'Erwan', 'Stanislas', 'Theodore', ' Armand', ' Moussa', 'Marc', 'Ilyes', ' Theophile', ' Lorenzo', ' Ryan', ' Diego', ' Pablo', ' Nils', ' Rafael', ' Sofiane', ' Alex', ' Vadim', ' Amir', ' Kylian', ' Jeremy', ' Gustave', ' Arsène', ' Loïc', ' Leopold', ' Cesar', ' Elliot', ' Mateo', ' Wassim', ' Nino', ' Anthony', ' Jonathan', ' Anton', ' François',
+            ' Dorian', ' Eliot', 'Remi', 'Milan', 'Mathys', 'Hamza', 'Marin', 'Henri', 'Maximilien', 'Ilian', 'Sami', 'Nathanaël', 'Nassim', 'Issa', 'Zakaria', 'Luc', 'Joachim', 'Bastien', 'Dimitri', ' Luka', ' Djibril', ' Ilan', ' Romeo', ' Olivier', ' Virgile', ' Kaïs', ' Auguste', ' Damien', ' Timothe', ' Noham', ' Alban', ' Bilal', ' Younes', ' Eric', ' Tiago'
+        ]
+
+let firstName_fem = [
+            'ouise', 'Camille', '  Emma', '  Inès', '  Chloe', '  Sarah', '  Alice', '  Lea', '  Juliette', '  Jeanne', '  Eva', '  Clara', '  Lina', '  Anna', '  Charlotte', '  Mathilde', '  Marie', '  Manon', '  Lucie', '  Anaïs', '  Jade', '  Zoe', '  Nina', '  Lou', '  Clemence', '  Pauline', '  Lisa', '  Adèle', '  Gabrielle', '  Romane', '  Julia', '  Victoria', '  Emilie', '  Rose', '  Julie', '  Margaux', '  Lena', '  Victoire', '  Lola', '  Valentine', '  Agathe', '  Alix', '  Elisa', '  Elise', '  Margot', '  Yasmine', '  Laura', '  Noa', '  Sofia', '  Noemie', '  Heloïse', '  Sara', '  Apolline', '  Salome', '  Diane', '  Maya', '  Ambre', '  Elsa', '  Josephine', '  Capucine', '  Ava', '  Sasha', '  Eleonore', '  Iris', '  Fatoumata', '  Alicia', '  Eden', '  Mila', '  Nour', '  Andrea', '  Garance', '  Aya', '  Violette', '  Constance', '  Justine', '  Melissa', '  Oceane', '  Olivia', '  Esther', '  Ines', '  Luna', '  Suzanne', '  Mariam', '  Maëlys', '  Myriam', '  Sophie', '  Celia', '  Lila', '  Madeleine', '  Marion', '  Anouk', '  Stella', '  Eloïse', '  Charlie', '  Leonie', '  Chiara', '  Carla', '  Blanche', '  Faustine', '  Claire', '  Lily', '  Roxane', '  Maëlle', '  Alexandra', '  Elena', '  Daphne', '  Clementine', '  Lise', '  Ella', '  Aminata', '  Romy', '  Aïcha', '  Kenza', '  Sophia', '  Lilou', '  Marine', '  Celeste', '  Lyna', '  Paloma', '  Lucile', '  Salma', '  Maïa', '  Melina', '  Sixtine', '  Amelie', '  Imane', '  Eve', '  Anaëlle', '  Lilia', '  Selma', '  Alma', '  Amandine', '  Eugenie', '  Louna', '  Celine', '  Raphaëlle', '  Ninon', '  Naomi', '  Manel', '  Elodie', '  Jasmine', '  Solène', '  Helène', '  Hanna', '  Angèle', '  Rachel', '  Tess', '  Thaïs', '  Albane', '  Berenice', '  Morgane', '  Sirine', '  Maria', '  Hortense', '  Chloé', '  Ana', '  Awa', '  Mia', '  Rebecca', '  Marguerite', '  Giulia', '  Alexia', '  Aurore', '  Isaure', '  Estelle', '  Philippine', '  Hannah', '  Nora', '  Audrey', '  Shirel', '  Clarisse', '  Amina', '  Lana', '  Assia', '  Rania', '  Hawa', '  Judith', '  Penelope', '  Rosalie', '  Alienor', '  Elea', '  Flora', '  Axelle', '  Farah', '  Lili', '  Louison', '  Coline', '  Astrid', '  Caroline', '  Fanny', '  Angelina', '  Zelie', '  Maïssa', '  Fatima', '  Jessica', '  Maryam', '  Leane', '  Dina', '  Bintou', '  Candice'
+        ]
+
+ let lastName = [
+            'Martin', 'Bernard', 'Dubois', 'Thomas', 'Robert ', 'Richard', 'Petit', 'Durand ', 'Leroy ', ' Moreau'
+        ]
+
+
+
+function generate(){
+    let xSex = Math.floor(Math.random()*2);
+    let xLN = Math.floor(Math.random()*10);
+    if(xSex === 1 ){
+        let xFN = Math.floor(Math.random()*197)
+        document.getElementById('identity').textContent = firstName_Mal[xFN].trim() +' '+ lastName[xLN].trim()
+    }else{
+        let xFN = Math.floor(Math.random()*200)
+        document.getElementById('identity').textContent = firstName_fem[xFN].trim() +' '+ lastName[xLN].trim()
+    }
+}
+
